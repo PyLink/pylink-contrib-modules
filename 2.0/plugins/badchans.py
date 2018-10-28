6efabadc9da8d97677d117575e047fcf41c5f188
James Lu <james@overdrivenetworks.com>
2018-10-28 13:15:33 -0700

    badchans: exempt local/reserved IPs & custom defined hosts

--- 2.0/plugins/badchans.py
+++ 2.0/plugins/badchans.py
@@ -30,13 +30,43 @@ def handle_join(irc, source, command, args):
                 asm_uid = world.services['antispam'].uids.get(irc.name)
 
             for user in args['users']:
+                try:
+                    ip = irc.users[user].ip
+                    ipa = ipaddress.ip_address(ip)
+                except (KeyError, ValueError):
+                    log.error("(%s) badchans: could not obtain IP of user %s", irc.name, user)
+                    continue
+                nuh = irc.get_hostmask(user)
+
+                exempt_hosts = set(conf.conf.get('badchans', {}).get('exempt_hosts', [])) | \
+                             set(irc.serverdata.get('badchans_exempt_hosts', []))
+
+                if not ipa.is_global:
+                    irc.msg(user, "Warning: %s kills unopered users, but non-public addresses are exempt." % channel,
+                            notice=True,
+                            source=asm_uid or irc.pseudoclient.uid)
+                    continue
+
+                if exempt_hosts:
+                    skip = False
+                    for glob in exempt_hosts:
+                        if irc.match_host(glob, user):
+                            log.info("(%s) badchans: ignoring exempt user %s on %s (%s)", irc.name, nuh, channel, ip)
+                            irc.msg(user, "Warning: %s kills unopered users, but your host is exempt." % channel,
+                                    notice=True,
+                                    source=asm_uid or irc.pseudoclient.uid)
+                            skip = True
+                            break
+                    if skip:
+                        continue
+
                 if irc.is_oper(user):
                     irc.msg(user, "Warning: %s kills unopered users!" % channel,
                             notice=True,
                             source=asm_uid or irc.pseudoclient.uid)
                 else:
                     log.info('(%s) badchans: killing user %s for joining channel %s',
-                             irc.name, irc.get_hostmask(user), channel)
+                             irc.name, nuh, channel)
                     irc.kill(asm_uid or irc.sid, user, REASON)
 
 utils.add_hook(handle_join, 'JOIN')
